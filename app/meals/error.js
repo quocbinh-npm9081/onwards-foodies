fcb9db32468808efdb734cdaebbaa79c299d40fe
BinhQuoc <quocbinhgithub@gmail>
2024-10-07 16:41:22 +0700

    112. Handling Errors

---
+++ app/meals/error.js
@@ -0,0 +1,10 @@
+'use client'
+export default function Error(props){
+    console.log(props)
+    return <main className="error">
+        <h1>An error occurred !</h1>
+        <p>
+           
+        </p>
+    </main>
+}
